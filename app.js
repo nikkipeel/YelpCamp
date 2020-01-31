2ac9738001cbe3a53c3d233558f3c1cfbc0242bb
nikkipeel <npeel3497@gmail.com>
2020-01-31 18:36:42 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,62 @@
+var express = require("express"),
+ 	app = express(),
+	bodyParser = require("body-parser"),
+	mongoose = require("mongoose"),
+	passport = require("passport"),
+	LocalStrategy = require("passport-local"),
+	methodOverride = require("method-override"),
+	flash = require("connect-flash"),
+	Campground = require("./models/campground"),
+	Comment = require("./models/comment"),
+	User = require("./models/user"),
+	seedDB = require("./seeds")
+
+	//requiring routes
+	var commentRoutes = require("./routes/comments"),
+		campgroundRoutes = require("./routes/campgrounds"),
+		indexRoutes = require("./routes/index");
+
+
+//seedDB(); //seed the DB
+//mongoose.connect("mongodb://localhost/yelp_camp_v12", {useNewUrlParser: true, useUnifiedTopology: true});
+//mongoose.connect("mongodb+srv://npeel:bdbdbd86@yelpcamp-zapwe.mongodb.net/test?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true});
+
+var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp_v12"
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true}); //|| ("mongodb+srv://npeel:bdbdbd86@yelpcamp-zapwe.mongodb.net/test?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true});
+//mongoose.connect(url);
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+app.use(flash());
+
+//PASSPORT CONFIGURATION
+app.use(require("express-session")({
+	secret: "Once again, Rusty wins cutest dog!",
+	resave: false,
+	saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+	res.locals.currentUser = req.user;
+	res.locals.error = req.flash("error");
+	res.locals.success = req.flash("success");
+	next();
+});
+
+app.use("/", indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
+
+
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log("Server Has Started!");
+});
